f0a741706f963bda7af34bd4884624671afdaa4e
MariaMiguel0510 <mariammpedro04@gmail.com>
2025-04-07 21:34:38 +0100

    scan background

--- JS/scan.js
+++ JS/scan.js
@@ -2,20 +2,21 @@ let camara;
 let time;
 
 function setup() {
-    createCanvas(innerWidth, innerHeight);
+    let canvas = createCanvas(540, 405);
+    canvas.parent('canvasCamara'); // adiciona o canvas dentro da div
     camara = createCapture(VIDEO, { flipped: true });
     camara.size(540, 405);
     camara.hide();
 
-    time = 10000; /*1 minuto é 10000*/
+    time = 100000; /*1 minuto é 10000*/
     setTimeout(muda, time); /*depois de passar um minuto aparece a parte de cair*/
 }
 
 function draw() {
     /*desenha a câmara centrada na janela*/
-    image(camara, width/2 - camara.width/2, height/2 - camara.height/2);
+    image(camara, width / 2 - camara.width / 2, height / 2 - camara.height / 2);
 }
 
-function muda(){
+function muda() {
     window.location.href = "rules.html";//é suposto ir para a parte de cair não das regras
 }
