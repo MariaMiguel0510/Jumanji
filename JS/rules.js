2d0608d344bf696276f0d174c986f9820a360607
MariaMiguel0510 <mariammpedro04@gmail.com>
2025-05-19 22:04:49 +0100

    queda na selva

--- JS/rules.js
+++ JS/rules.js
@@ -1,40 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let savedNickname = localStorage.getItem("nickname");
+// Espera o vídeo terminar para mostrar o conteúdo
+const video = document.getElementById("introVideo");
+const content = document.getElementById("mainContent");
 
-    if (savedNickname) {
-        let nicknameDisplay = document.getElementById("nicknameDisplay");
-        nicknameDisplay.textContent = savedNickname;
-    } else {
-        console.log("Nenhum nickname encontrado.");
-    }
+video.addEventListener("ended", () => {
+    window.location.href = "nivel1.html";
 });
 
-//desenha as regras de x em x segundos, para que apreça uma parte do texto de cada vez na tela
-function mostraTexto() {
-    let texto = document.querySelectorAll('.rules');
-    let nL = 3; // número de linhas de texto (h2)
-    let i = 0; // variável para controlar qual dos h2 é exibido 
+// Exibe o nickname salvo
+const nickname = localStorage.getItem("nickname");
+document.getElementById("nicknameDisplay").textContent = nickname ? " " + nickname : "";
 
-    function escreve() {
-
-        if (i < nL) { // Verifica se o índice está dentro do limite
-            // Esconde todas as frases usando um ciclo for
-            for (let j = 0; j < nL; j++) {
-                texto[j].style.display = 'none';
-            }
-
-            // Exibe a frase atual
-            texto[i].style.display = 'block';
-            i++; // Incrementa o índice para o próximo texto
-        }
-    }
-
-    // mostra o primeiro h2
-    escreve();
-
-    // define o intervalo para trocar os h2 a cada 5 segundos
-    setInterval(escreve, 5000);
-}
-
-// Executa a função após o carregamento da página
-window.onload = mostraTexto;
